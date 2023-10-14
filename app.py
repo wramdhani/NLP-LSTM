042b8feba4674984fda5efa33cbf2de3605adfa9
wramdhani <anakpintar2003@gmail.com>
2023-10-14 19:41:52 +0700

    v1.0.1

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import render_template, Flask
-
+from process import model
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('website/index.html')
 
 if __name__ == '__main__':
     app.run(host="localhost", port=8080)
