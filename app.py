c59f07e6b7e4245089770eb4a70fedd1e55b9c48
wramdhani <anakpintar2003@gmail.com>
2023-11-28 20:23:00 +0700

    waly

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, session
+from flask_session import Session  # Import the Session object
 from process import preparation, generate_response
 
 app = Flask(
@@ -7,17 +9,24 @@ app = Flask(
     static_folder="website/static"
 )
 
+# Set the secret key and configure the session to use filesystem storage
+app.config['SECRET_KEY'] = os.urandom(16)
+app.config['SESSION_TYPE'] = 'filesystem'
+
+Session(app)  # Initialize the session
+
 #download nltk
 preparation()
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home():
-    return render_template("chatInterface.html")
-
-@app.route('/get')
-def get_bot_response():
-    user_input = request.args.get('msg')
-    return str(generate_response(user_input))
+    if 'messages' not in session:
+        session['messages'] = []
+    if request.method == 'POST':
+        user_input = request.form['msg']
+        session['messages'].append({'class': 'user-message', 'text': user_input})
+        session['messages'].append({'class': 'bot-message', 'text': generate_response(user_input)})
+    return render_template("chatInterface.html", messages=session['messages'])
 
 if __name__ == "__main__":
     app.run(
