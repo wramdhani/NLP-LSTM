374bae349c5706f564a62b659d9eeb5966e358e6
wramdhani <anakpintar2003@gmail.com>
2023-11-28 16:31:36 +0700

    waly

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
-import msvcrt
-import os
+from flask import Flask, render_template, request
 from process import preparation, generate_response
+
+app = Flask(
+    __name__,
+    template_folder="website/templates",
+    static_folder="website/static"
+)
 
 #download nltk
 preparation()
+
+@app.route('/')
+def home():
+    return render_template("chatInterface.html")
+
+@app.route('/get')
 def get_bot_response():
-    user_input = input("Kamu: ")
-    result = generate_response(user_input)
-    return result
+    user_input = request.args.get('msg')
+    return str(generate_response(user_input))
 
 if __name__ == "__main__":
-    os.system('cls')
-    while True:
-        response = get_bot_response()
-        print(f"Cece: {response}")
-
-        if msvcrt.kbhit() and msvcrt.getch() == b'\x1b':
-            print("Conversation ended")
-            break
-        
+    app.run(
+        host="localhost",
+        port=8080,
+        debug=True
+    )
